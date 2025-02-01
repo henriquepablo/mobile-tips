3423fa13e8d8191c88fa4ff371d8d5ae49ea891f
henriquepablo <pablo269704@gmail.com>
2025-02-01 20:00:00 -0300

    Initial Commit

---
+++ MobileTips/MobileTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTipsApp.swift
+//  MobileTips
+//
+//  Created by pablo henrique on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MobileTipsTests/MobileTipsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTipsTests.swift
+//  MobileTipsTests
+//
+//  Created by pablo henrique on 01/02/25.
+//
+
+import Testing
+@testable import MobileTips
+
+struct MobileTipsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
